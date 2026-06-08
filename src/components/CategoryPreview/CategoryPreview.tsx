1df28fb15e341ea13cb8673676c1eaa6fdb93558
Desokir <ska866234@gmail.com>
2026-06-08 10:38:13 +0300

    add

---
+++ src/components/CategoryPreview/CategoryPreview.tsx
@@ -0,0 +1,12 @@
+import type { FC } from 'react';
+import './CategoryPreview.css';
+
+interface CategoryPreviewProps {}
+
+const CategoryPreview: FC<CategoryPreviewProps> = () => (
+  <div className="CategoryPreview">
+    CategoryPreview Component
+  </div>
+);
+
+export default CategoryPreview;
