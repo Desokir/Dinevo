7feaa6424d7610aeebf083fdf938b7744528ee75
sasasasasasaasasassa <sasaistomin42@gmail.com>
2026-06-09 23:40:58 +0300

    create footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,41 @@
-
 import Benefit from "@/src/components/Benefit/Benefit";
+import Footer from "@/src/components/Footer/Footer";
 import Header from "@/src/components/Header/Header";
 import Link from "next/link";
+
+const linksFooter = {
+    'l1': {
+        'name': 'Головна',
+        'link': '/'
+    },
+    'l2': {
+        'name': 'Меню',
+        'link': '/menu'
+    },
+    'l3': {
+        'name': 'Доставка',
+        'link': '/delivery'
+    },
+    'l4': {
+        'name': 'Контакти',
+        'link': '/contacts'
+    }
+}
+const categoriFooter = ['Ресторани', 'Супермаркети', 'Аптеки', 'Квіти']
+const contacts = {
+    'Number': '+380956848943',
+    'Email': 'dinevo2026@gmail.ua'
+}
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
        <Header></Header> 
       
-      <div>
+      <div> 
       <Benefit></Benefit>
       </div>
+      <Footer links={linksFooter} categories={categoriFooter} contact={contacts} />
     </main>
-  );
+  );    
 }
->>>>>>> 19d4a1cc0e23b04e94dea7fe83c341785a0dabc1
