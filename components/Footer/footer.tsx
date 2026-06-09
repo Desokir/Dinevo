759cd6966c6c9c0fb4efbc3e37617c17e708efbc
sasasasasasaasasassa <sasaistomin42@gmail.com>
2026-06-09 11:41:15 +0300

    add

---
+++ components/Footer/footer.tsx
@@ -0,0 +1,7 @@
+interface Props
+
+function Footer() {
+    return (
+        
+    );
+}
---
+++ components/OutResturant/out.tsx
@@ -0,0 +1,14 @@
+interface Props {
+    logo: string,
+    name: string
+}
+
+function OutLine() {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default OutLine;
