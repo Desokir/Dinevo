61624073399d042e202f0f24d277d943df69e80b
Desokir <ska866234@gmail.com>
2026-06-09 13:40:07 +0300

    add  props navigate

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,16 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
-       <Header></Header> 
+       <Header
+      logoSrc="/LogoOrange.png"
+      navLinks={[
+        { label: "Головна", href: "/" },  
+        { label: "Меню", href: "/menu" },
+        { label: "Доставка", href: "/delivery" },
+        { label: "Контакти", href: "/contacts" }
+      ]}
+       buttonText="зареєструватися"
+    />
       
       <div>
       <Benefit></Benefit>
