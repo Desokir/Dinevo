cd8b7c19c9acfbb7b5fcbf553e0022a4233e450a
sasasasasasaasasassa <sasaistomin42@gmail.com>
2026-06-10 11:21:01 +0300

    init

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,34 @@
 import './Header.css';
 import Link from "next/link";
 interface Props {
-  logoSrc: string;
-  buttonText: string;
-  navLinks: {
-    label: string;
-    href: string;
-  }[];
+    logoSrc: string;
+    buttonText: string;
+    navLinks: {
+        label: string;
+        href: string;
+    }[];
 }
 
 export default function Header({ logoSrc, buttonText, navLinks }: Props) {
-  return (
-    <header className="header">
-      <div className="container">
-        <Link href="/" className="logo">
-          <img src={logoSrc} alt="logo" />
-        </Link>
+    return (
+        <header className="header">
+            <div className="container">
+                <Link href="/" className="logo">
+                    <img src={logoSrc} alt="logo" />
+                </Link>
 
-        <nav className="nav">
-          {navLinks.map((item, index) => (
-            <Link key={index} href={item.href}>
-              {item.label}
-            </Link>
-          ))}
-        </nav>
+                <nav className="nav">
+                    {navLinks.map((item, index) => (
+                        <Link key={index} href={item.href}>
+                            {item.label}
+                        </Link>
+                    ))}
+                </nav>
 
-        <button className="order-btn">
-          {buttonText}
-        </button>
-      </div>
-    </header>
-  );
+                <button className="order-btn">
+                    {buttonText}
+                </button>
+            </div>
+        </header>
+    );
 }
